f81aec9fbb9f8baebf4396d558ffd79b9b54cc9c
Zhenis <zhenis.serkebaev@gmail.com>
2024-09-26 13:38:54 +0500

    config vite: add scss modern compiler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,24 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@vee-validate/nuxt"],
-  css: ["vue-final-modal/style.css", 
-    '~/assets/styles/main.scss',
-  ],
+  css: ["vue-final-modal/style.css", "~/assets/styles/main.scss"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler", // or "modern"
+        },
+      },
+    },
+  },
+
+  compatibilityDate: "2024-09-26",
 });
